50752cb80394203bb2a7a817017fee550dcfd1f8
FlammingOstrich <39203620+FlammingOstrich@users.noreply.github.com>
2023-01-01 19:43:20 -0500

    spring initializer starter

---
+++ stargazer/src/main/java/com/omerygouw/stargazer/StargazerApplication.java
@@ -0,0 +1,13 @@
+package com.omerygouw.stargazer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StargazerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StargazerApplication.class, args);
+	}
+
+}
---
+++ stargazer/src/test/java/com/omerygouw/stargazer/StargazerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omerygouw.stargazer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StargazerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
