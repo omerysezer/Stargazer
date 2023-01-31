1c76b3f1ab9777777f5e2e8446cf432a9190d196
Omer Sezer <omerysezer@gmail.com>
2023-01-31 02:13:06 -0500

    implemented client side of pairing with rpi

--- stargazer/src/main/resources/static/js/pairing.js
+++ stargazer/src/main/resources/static/js/pairing.js
@@ -1,33 +1,56 @@
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        500
+    )
+});
+
 function pair(numSideways, numVertical, numOnOff) {
-    let piId = "" + numSideways + "" + numVertical + "" + numOnOff;
-    let oldHeader = structuredClone(standardHeader);
-    let oldSessionId = oldHeader.sessionId;
-    let newSubscriptionId = sessionId + piId;
+    let submitButton = document.getElementById("submitButton");
+    submitButton.disabled = true;
 
-    client.publish({
-        destination: "/api/pair",
-        body: piId,
-        headers: oldHeader
-    });
+    let output = document.getElementById("pairingResultOutput");
+    let sidewaysTurns = document.getElementById("slider1").value;
+    let verticalTurns = document.getElementById("slider2").value;
+    let laserBlinks = document.getElementById("slider3").value;
 
-    changeSessions(newSubscriptionId);
-    awaitingResponseQueue.push(processResponse);
+    let piId = String(sidewaysTurns) + String(verticalTurns) + String(laserBlinks);
 
-    function processResponse(response) {
-        console.log(response);
-        if (!String(response.body.status) === "SUCCESS") {
-            alert("Pairing Was Unsuccessful: " + response.body.reason);
-            changeSessions(oldSessionId);
+    let oldSessionId = socketHandler.getSessionId();
+    let newSessionId = oldSessionId + piId;
+
+    let message = JSON.stringify({"piSessionId": piId});
+    let destination = "/api/pair";
+
+    socketHandler.sendMessage(message, destination, handleResponse);
+
+    function handleResponse(response) {
+        if (String(response.status) !== "SUCCESS") {
+            output.innerHTML = "<b>Failed to pair with the Raspberry Pi.<br>Server Response: \"" + response.message +"\"</b>";
         }
-    }
+        else{
+            let promise = new Promise(function(resolve, reject){
+                try{
+                    socketHandler.changeSubscription(newSessionId);
+                    resolve();
+                }
+                catch(err){
+                    socketHandler.changeSubscription(oldSessionId);
+                    reject();
+                }
+            });
 
-    function changeSessions(newSessionId){
-        document.cookie = "sessionId=" + newSessionId;
-        sessionId = newSessionId;
-        standardHeader.sessionId = newSessionId;
-        subscription.unsubscribe();
-        subscription = client.subscribe("/user/queue/session-" + newSessionId, incomingMessageHandler);
+            promise.then(function(){
+                output.innerHTML = "<b>Successfully paired with the Raspberry Pi! Press the continue button.</b>";
+                submitButton.innerText = "Continue";
+                submitButton.onclick = () => document.location = 'location.html';
+            }).catch(function(){
+                output.innerHTML = "<b>Successfully paired to Raspberry Pi but could not reestablish connection to server.</b>";
+            });
 
-        waitForConnection(() => {});
+            submitButton.disabled = false;
+        }
     }
 }
